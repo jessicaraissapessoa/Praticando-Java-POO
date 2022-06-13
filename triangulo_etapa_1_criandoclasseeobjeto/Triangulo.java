57ab78cdc83d7d0f1e91c9b156fd361d26bfb5ac
Jéssica Raissa Pessoa <jessicaraissapessoa@gmail.com>
2022-06-13 14:11:03 -0300

    Add files via upload

---
+++ triangulo_etapa_1_criandoclasseeobjeto/Triangulo.java
@@ -0,0 +1,20 @@
+package triangulo_etapa_1_criandoclasseeobjeto;
+
+public class Triangulo {
+
+	//Triângulos a, b e c
+	//Base (x), Altura (y)
+	//a(x, y) -> ax, ay / b (x, y) -> bx, by / c (x, y) -> cx, cy
+		
+	public double ladoa;
+	public double ladob;
+	public double ladoc;
+	
+}
+	
+	
+	
+	
+		
+		
+
