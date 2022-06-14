6ccf6893cd6623cd3653a0aa7bfdd2db777e37eb
Jéssica Raissa Pessoa <jessicaraissapessoa@gmail.com>
2022-06-14 17:58:04 -0300

    Add files via upload

---
+++ treinando POO proprietário-carro/Carro.java
@@ -0,0 +1,13 @@
+package treinandoPOOproprietáriocarro;
+
+public class Carro {
+
+	String fabricante;
+	String modelo;
+	String cor;
+	String tipocombustivel;
+	int anofabricacao;
+	double valormercado;
+	Proprietario dono;
+	
+}
---
+++ treinando POO proprietário-carro/Proprietario.java
@@ -0,0 +1,14 @@
+package treinandoPOOproprietáriocarro;
+
+public class Proprietario {
+
+	String nomeproprietario;
+	String cpf;
+	int idade;
+	String logradouro;
+	String numerologradouro;
+	String bairro;
+	String cidade;
+	String estado;
+	
+}
